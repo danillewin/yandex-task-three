30d29d825895e6bb273fca77d4b9291e10f5d087
Danil Lewin <danillewin@gmail.com>
2015-08-07 22:12:30 +0400

    naive and buggy implementation of sound playing

--- script/app/app.js
+++ script/app/app.js
@@ -1,57 +1,78 @@
 app = {};
+
 app.vm = {};
+
+try {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    app.AudioContext = new AudioContext();
+}
+catch(e) {
+    alert('Web Audio API is not supported in this browser');
+}
+
 
 (function (app) {
 
     app.vm.TrackList = function () {
-    var self = this;
+        var self = this;
 
-    self.tracks = ko.observableArray([]);
+        self.tracks = ko.observableArray([]);
 
-    self.initDragDrop();
-}
+        self.initDragDrop();
+    }
 
     app.vm.TrackList.prototype.addTrack = function (file) {
-    var self = this,
-        tags,
-        track,
-        image,
-        base64String = "",
-        imgUrl;
+        var self = this,
+            tags,
+            track,
+            image,
+            base64String = "",
+            imgUrl,
+            reader = new FileReader(),
+            context = app.AudioContext;
 
-    ID3.loadTags(file, function() {
-        tags = ID3.getAllTags(file);
+        ID3.loadTags(file, function() {
+            tags = ID3.getAllTags(file);
 
-        if (tags.picture) {
 
-            image = tags.picture;
+            if (tags.picture) {
 
-            for (var i = 0; i < image.data.length; i++) {
-                base64String += String.fromCharCode(image.data[i]);
+                image = tags.picture;
+
+                for (var i = 0; i < image.data.length; i++) {
+                    base64String += String.fromCharCode(image.data[i]);
+                }
+
+                imgUrl = "data:" + image.format + ";base64," + window.btoa(base64String);
+            }
+            else {
+                imgUrl = "images/unknown.png"
             }
 
-            imgUrl = "data:" + image.format + ";base64," + window.btoa(base64String);
-        }
-        else {
-            imgUrl = "images/unknown.png"
-        }
+            track = {
+                artist: tags.artist || "unknown",
+                title: tags.title || "unknown",
+                album: tags.album || "unknown",
+                year: tags.year,
+                lyrics: tags.lyrics,
+                picture: imgUrl,
+            }
 
-        track = {
-            artist: tags.artist || "unknown",
-            title: tags.title || "unknown",
-            album: tags.album || "unknown",
-            year: tags.year,
-            lyrics: tags.lyrics,
-            picture: imgUrl
-        }
+            reader.onload = function () {
+                context.decodeAudioData(reader.result, function(audio){
+                    track.audio = audio;
+                });
 
-        self.tracks.push(track);
-    },
-    {
-        tags: ["artist", "title", "album", "year", "lyrics", "picture"],
-        dataReader: FileAPIReader(file)
-    });
-}
+                self.tracks.push(track);
+            };
+
+            reader.readAsArrayBuffer(file);
+        },
+        {
+            tags: ["artist", "title", "album", "year", "lyrics", "picture"],
+            dataReader: FileAPIReader(file)
+        });
+    }
 
     app.vm.TrackList.prototype.initDragDrop = function () {
     var self = this;
@@ -99,12 +120,17 @@ ko.applyBindings(app.TrackList, document.getElementsByClassName("track-list")[0]
     app.vm.Player = function () {
         var self = this;
 
-        self.mode = ko.observable();
+        self.context = app.AudioContext;
+        self.currentTime = ko.observable(0);
+        self.source = self.context.createBufferSource();
+        self.playing = ko.observable(false);
         self.vol = ko.observable();
         self.repeat = ko.observable();
         self.shuffle = ko.observable();
         self.equalizer = ko.observable();
-
+        self.currentTrack = ko.observable();
+        self.currentTrack = ko.observable();
+        self.tracks = app.TrackList.tracks;
         self.presets = {
             jazz : "",
             normal: "",
@@ -112,14 +138,45 @@ ko.applyBindings(app.TrackList, document.getElementsByClassName("track-list")[0]
             pop: ""
         }
 
-        self.tracks = app.TrackList.tracks;
+        self.tracks.subscribe(function () {
+            if (self.tracks().length == 1) {
+                self.currentTrack(self.tracks()[0])
+            }
+        });
+    }
+
+    app.vm.Player.prototype.playPause = function () {
+        var self = this;
+
+        if (!self.playing()) {
+            self.play();
+        }
+        else {
+            self.pause();
+        }
     }
 
     app.vm.Player.prototype.pause = function () {
+        var self = this;
 
+        self.source.stop();
+        self.playing(false);
     }
 
     app.vm.Player.prototype.play = function () {
+        var self = this;
+
+        if (self.currentTrack()){
+            if (!self.source.buffer) {
+                self.source.buffer = self.currentTrack().audio;
+                self.source.connect(self.context.destination);
+            }
+            self.source.start(0);
+            self.playing(true);
+        }
+        else {
+            alert("Upload the track first");
+        }
 
     }
 
@@ -132,4 +189,8 @@ ko.applyBindings(app.TrackList, document.getElementsByClassName("track-list")[0]
     }
 
 })(window.app);
+
+app.Player = new app.vm.Player();
+
+ko.applyBindings(app.Player, document.getElementsByClassName("controls")[0]);
 
